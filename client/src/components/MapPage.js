214ddf40332649d22d822e922d4233c9342615b5
giongto35 <giongto35@yahoo.com>
2016-09-09 14:24:35 +0800

    Add my locaiton

--- client/src/components/MapPage.js
+++ client/src/components/MapPage.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/Map.css';
 import ReactMapboxGl from "react-mapbox-gl";
 // import { Layer, Feature } from "react-mapbox-gl";
 
-const position = [103.8198, 1.3224];
+function geoListener(callback) {
+  navigator.geolocation.watchPosition(
+    ({ coords, timestamp }) => callback(coords),
+    (err) => alert('Unable to find position - ' + err.message), 
+    {
+      enableHighAccuracy: true,
+      timeout: 15000
+    }
+  )
+}
 
-const MapPage = props => (
-  <div>
-    <ReactMapboxGl
-      style="mapbox://styles/mapbox/streets-v8"
-      accessToken={process.env.REACT_APP_MAPBOX_API_KEY}
-      zoom={[16]}
-      pitch={50}
-      center={position}
-    />
-  </div>
-);
+class MapPage extends Component {
+  state = {
+    location: [103.8198, 1.3224]
+  }
+
+  updateLocation(coords) {
+    console.log(coords);
+    this.setState({
+      center: [coords.longitude, coords.latitude]
+    });
+  }
+
+  componentWillMount() {
+    geoListener(this.updateLocation.bind(this));
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactMapboxGl
+          style="mapbox://styles/mapbox/streets-v8"
+          accessToken='pk.eyJ1IjoiZ2lvbmd0bzM1IiwiYSI6ImNpc3NtdzJ1ODAwaGMyb29hbzU1bnY4amUifQ.vt5Dk7_fo_hROC84-DKsrw'
+          zoom={[16]}
+          pitch={50}
+          center={this.state.center}
+        />
+      </div>
+    );
+  }
+}
 
 export default MapPage;
