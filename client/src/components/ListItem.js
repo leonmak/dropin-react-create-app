56b5b8244364b1edc991e713dd151b4549122ff1
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-09-09 20:48:59 +0800

    update to google material ui font for unified feel

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import '../styles/ListItem.css'
+import * as Icons from '../utils/Icons';
 
 export class ListItem extends Component{
 
@@ -18,8 +19,8 @@ export class ListItem extends Component{
 			<div id="container">
 
 			<div>
-			<p id="replies">{this.props.replies} replies</p>
-			<p id="distance">{this.props.distance} from you</p>
+			<p id="replies">{Icons.MUI('chat')}{this.props.replies} replies</p>
+			<p id="distance">{Icons.MUI('place')}{this.props.distance} from you</p>
 			</div>
 			
 			<div id="votes">
