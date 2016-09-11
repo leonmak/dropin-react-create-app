b9ae57b91b7569aac18fe7bce582f75e3aa33831
kaiyisg <kaiyisg@yahoo.com.sg>
2016-09-11 19:28:45 +0800

    added react-headroom for header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react';
 import {BottomBar} from './BottomBar';
+import {TopBar} from './TopBar';
+import Headroom from 'react-headroom';
 
 const App = (props) => {
 	return (
-		<div id="holder">
+	<div id="holder">
+		<Headroom><TopBar/></Headroom>
   		<div id="body">{props.children}</div>
   		<footer><BottomBar url={props.location.pathname} /></footer>
   	</div>
