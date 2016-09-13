ecb1604f169316a6508afdcdbc0b8342838b74e2
giongto35 <giongto35@yahoo.com>
2016-09-13 14:28:21 +0800

    Add Users API

--- server/controller/UsersController.js
+++ server/controller/UsersController.js
@@ -1,4 +1,5 @@
 import { Users } from '../database';
+const ERROR_NOT_FOUND = "Not found";
 
 var UserController = {};
 
@@ -7,31 +8,32 @@ UserController.getUsers = function(req, res) {
 		res.json(users.toJSON());
 	}).catch(function(err) {
 		res.json({error: err});
-		console.error(err);
 	});
 }
 
 UserController.getUser = function(req, res) {
-	id = req.id;
+	const id = req.params.id;
 	Users.where('id', id).fetch().then(function(user) {
 		res.json(user.toJSON());
 	}).catch(function(err) {
-		res.json({error: err});
-		console.error(error);
+		res.json({error: ERROR_NOT_FOUND});
 	})
 }
 
 UserController.createUser = function(access_token, profile) {
 	Users.where('facebook_id', profile.id).fetch().then(function(user) {
-			console.log(user);
 		if (user) {
 			// update access token
-			user.save({'facebook_token': access_token});
+			user.save({
+				facebook_token: access_token,
+				facebook_profile_img: profile.photos.length > 0 ? profile.photos[0].value : null
+			});
 		} else {
 			// create new one
 			const userHash = {
 				facebook_id: profile.id,
 				facebook_name: profile.displayName,
+				facebook_profile_img: profile.photos.length > 0 ? profile.photos[0].value : null,
 				facebook_token: access_token
 			}
 			new Users().save(userHash);
