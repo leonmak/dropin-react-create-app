bbfb7f82cb5a3fa3961b47a0f052e2c4289a093c
kaiyisg <kaiyisg@yahoo.com.sg>
2016-09-13 13:13:05 +0800

    adding seed data for drop page

--- client/src/components/DropPage/Drop.js
+++ client/src/components/DropPage/Drop.js
@@ -2,16 +2,10 @@ import React, {Component} from 'react';
 
 export class Drop extends Component {
 
-	/*componentDidMount() {
-		this.setState({
-			dropId: findUserById(this.props.params.userId)
-		})
-	},*/
-
 	render() {
 		return (
 		<div>
-			<h2>hello</h2>
+		{this.props.drop.title}
 		</div>
 		)
 	}
--- client/src/components/DropPage/index.js
+++ client/src/components/DropPage/index.js
@@ -1,23 +1,86 @@
 import React from 'react';
 import {Drop} from './Drop';
 
-/*const DropPage = (props) => (
-  <Drop/>
-)
+//api call used here
+// /api/comments/:dropid/comments
 
-export default DropPage;*/
+//sorted in chronological order	
+
+var comments = [
+{
+	"id":"001",
+	"username":"Leon",
+	"userId":"001",
+	"userAvatar":"http://dropdev.com/avatar/001",
+	"text":"maybe you should czech the fridge",
+	"createdAt": "2016-08-23T18:25:43.511Z"
+},
+{
+	"id":"002",
+	"username":"Thanh",
+	"userId":"002",
+	"userAvatar":"http://dropdev.com/avatar/002",
+	"text":"im russian to the kitchen",
+	"createdAt": "2016-08-23T18:49:43.511Z"
+},
+{
+	"id":"003",
+	"username":"Leon",
+	"userId":"001",
+	"userAvatar":"http://dropdev.com/avatar/001",
+	"text":"maybe you will find some turkey",
+	"createdAt": "2016-08-23T18:51:22.664Z"
+},
+{
+	"id":"004",
+	"username":"Thanh",
+	"userId":"002",
+	"userAvatar":"http://dropdev.com/avatar/002",
+	"text":"we have some but it is covered in a layer of greece",
+	"createdAt": "2016-08-23T19:01:22.664Z"
+},
+{
+	"id":"005",
+	"username":"anon",
+	"userId":"-1",
+	"userAvatar":"http://dropdev.com/avatar/anon",
+	"text":"i think i'll settle for some chile",
+	"createdAt": "2016-08-23T19:04:22.664Z"
+},
+{
+	"id":"006",
+	"username":"anon",
+	"userId":"-1",
+	"userAvatar":"http://dropdev.com/avatar/anon",
+	"text":"that sounds appetizing, i would love a canada chile as well",
+	"createdAt": "2016-08-23T19:04:27.664Z"
+},
+{
+	"id":"007",
+	"username":"Kai Yi",
+	"userId":"003",
+	"userAvatar":"http://dropdev.com/avatar/003",
+	"text":"denmark your name on it",
+	"createdAt": "2016-08-24T00:00:35.664Z"
+}
+];
 
 
-const DropPage = React.createClass({
+//not sure how to get this data from the other page, passing data around with react-router?
+var drop = {
+    "id": "001",
+    "emojiUni": "1f600", //utf-8 encoded sleepy face
+    "title": "I cannot find any food today!!!",
+    "votes": 4,
+    "distance": 1560,
+    "time": "2016-08-23T18:25:43.511Z",
+    "replies": 7
+}
 
-  render() {
-    return (
-      <div>
-        <h2>hello</h2>
-        {console.log(this.props.dropId)}
-      </div>
-    )
-  }
-})
+
+const DropPage = (props) => (
+	<Drop comments={comments}
+	drop={drop}/>
+	)
 
 export default DropPage;
