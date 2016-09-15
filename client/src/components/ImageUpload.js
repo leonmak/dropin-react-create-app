78f81deedf66027c423e90a6972bdea7a29e8696
leon mak <leonmakansheng@gmail.com>
2016-09-15 12:29:49 +0800

    Added cloudinary component

---
+++ client/src/components/ImageUpload.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+// import './.css';
+
+export default class ImageUpload extends Component {
+  constructor(props) {
+    console.log(process.env.REACT_APP_CLOUDINARY_UPLOAD_URL)
+    super(props);
+
+    this.state = {
+      uploadedFile: null,
+      uploadedFileCloudinaryUrl: ''
+    };
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    console.log(file)
+    let upload = request.post(process.env.REACT_APP_CLOUDINARY_UPLOAD_URL)
+                     .field('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET)
+                     .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        console.log(response.body)
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <div>
+          <Dropzone
+            onDrop={this.onImageDrop.bind(this)}
+            multiple={false}
+            accept="image/*">
+            <div>Drop an image or click to select a file to upload.</div>
+          </Dropzone>
+        </div>
+
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === '' ? null :
+          <div>
+            <p>{this.state.uploadedFile.name}</p>
+            <img alt={this.state.uploadedFileCloudinaryUrl} src={this.state.uploadedFileCloudinaryUrl} />
+          </div>}
+        </div>
+      </form>
+    )
+  }
+}
--- client/src/components/SettingsPage/index.js
+++ client/src/components/SettingsPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import * as fb from '../../utils/facebook-url';
+import ImageUpload from '../ImageUpload';
 
 const SettingsPage = (props) => (
   <div className="row center-xs profile-container bg-cyan-300">
@@ -9,7 +10,9 @@ const SettingsPage = (props) => (
         src={fb.profileImg(props.user.facebookId, 90)}
         size={100}
       />
+
     </div>
+      <ImageUpload />
   </div>
 )
 
