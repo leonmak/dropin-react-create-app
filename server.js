32d7f80294f27eb702b629a1c3885262bbf6c9f6
leon mak <leonmakansheng@gmail.com>
2016-09-20 15:44:17 +0800

    Req express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
