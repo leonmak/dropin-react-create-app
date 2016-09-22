d136557ea5f706b2278877c6db3a71ebfb1f24c6
leon mak <leonmakansheng@gmail.com>
2016-09-22 12:23:44 +0800

    Add spinner to comments

--- client/src/components/CommentsList/index.js
+++ client/src/components/CommentsList/index.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
 import ScrollArea from 'react-scrollbar';
-
-
-
+import CircularProgress from 'material-ui/CircularProgress';
 
 export class CommentsList extends Component {
 
@@ -11,27 +9,26 @@ export class CommentsList extends Component {
     location: [103.8198, 1.3224]
   }
 
-
-
   render() {
-
-    //console.log(this.props.comments);
 
     return (
       <div>
-      {this.props.comments.map(comment => {
-        let {text, created_at, username, id, userId, userAvatarId, dropId} = comment;
-        return <Comment
-        key={id}
-        text={text}
-        dropId={dropId}
-        username={username}
-        userId={userId}
-        userAvatar={userAvatarId}
-        createdAt={created_at}
-        isProfile={this.props.isProfile} />
+      {this.props.comments.length > 0 ?
+        this.props.comments.map(comment => {
+          let {text, created_at, username, id, userId, userAvatarId, dropId} = comment;
+          return <Comment
+          key={id}
+          text={text}
+          dropId={dropId}
+          username={username}
+          userId={userId}
+          userAvatar={userAvatarId}
+          createdAt={created_at}
+          isProfile={this.props.isProfile} />
+        })
+        : <CircularProgress className="spinner"/>
       }
-      )}
+
       </div>
       );
   }
